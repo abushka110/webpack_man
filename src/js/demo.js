eaa382e95ab4210a9e0b21a83b36e1f26699ca48
Oleh Donets <qyjodixilu@gmail.com>
2023-03-02 00:35:36 +0200

    swiper added

--- src/js/demo.js
+++ src/js/demo.js
@@ -1,4 +1,13 @@
 const demo = () => 'Webpack Boilerplate v5.15.0 - SASS/PostCSS, ES6/7, browser sync, source code listing and more.';
 
+// swiper
+const sliderParameters = {
+    loop: true,
+    slidesPerView: 4,
+    spaceBetween: 30,
+};
+
+const swiper = new Swiper('#features-slider', sliderParameters);
+
 // eslint-disable-next-line no-console
 console.log(demo());
