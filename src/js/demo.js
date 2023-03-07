8950ec88cc37f818b8154e559d91d01f3411e798
Oleh Donets <qyjodixilu@gmail.com>
2023-03-07 15:02:25 +0200

    minor changes

--- src/js/demo.js
+++ src/js/demo.js
@@ -2,21 +2,21 @@ const demo = () => 'Webpack Boilerplate v5.15.0 - SASS/PostCSS, ES6/7, browser s
 
 // swiper
 const sliderParameters = {
-    loop: true,
-    slidesPerView: 4,
-    spaceBetween: 30,
+  loop: true,
+  slidesPerView: 4,
+  spaceBetween: 30,
 };
 
 const swiper = new Swiper('#features-slider', sliderParameters);
 
 // slick
-$(document).ready(function(){
-    $('.slick-slider').slick({
-      autoplay: true,
-      slidesToShow: 4,
-      slidesToScroll: 4,
-      infinite: true,
-    });
+$(document).ready(function () {
+  $('.slick-slider').slick({
+    autoplay: true,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    infinite: true,
   });
+});
 // eslint-disable-next-line no-console
 console.log(demo());
