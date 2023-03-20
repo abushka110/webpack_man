ab2ef73caa5280977cd48343df5087dc18fef6d4
Oleh Donets <qyjodixilu@gmail.com>
2023-03-21 01:06:25 +0200

    slick was removed

--- src/js/demo.js
+++ src/js/demo.js
@@ -1,4 +1,5 @@
-const demo = () => 'Webpack Boilerplate v5.15.0 - SASS/PostCSS, ES6/7, browser sync, source code listing and more.';
+const demo = () =>
+  "Webpack Boilerplate v5.15.0 - SASS/PostCSS, ES6/7, browser sync, source code listing and more.";
 
 // swiper
 const sliderParameters = {
@@ -7,16 +8,7 @@ const sliderParameters = {
   spaceBetween: 30,
 };
 
-const swiper = new Swiper('#features-slider', sliderParameters);
+const swiper = new Swiper("#features-slider", sliderParameters);
 
-// slick
-$(document).ready(function () {
-  $('.slick-slider').slick({
-    autoplay: true,
-    slidesToShow: 4,
-    slidesToScroll: 4,
-    infinite: true,
-  });
-});
 // eslint-disable-next-line no-console
 console.log(demo());
